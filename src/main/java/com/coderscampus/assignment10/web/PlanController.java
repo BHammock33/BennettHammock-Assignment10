b74f820d8b351ba84460b514802ce52cbf89cd1f
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-11-30 15:35:06 -0500

    Update PlanController.java

--- src/main/java/com/coderscampus/assignment10/web/PlanController.java
+++ src/main/java/com/coderscampus/assignment10/web/PlanController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,9 +15,11 @@ import com.coderscampus.assignment10.dto.WeekResponse;
 @RestController
 public class PlanController {
 
+	String Key = "700e9a8811454149bcb551d8ee52aff5";
+	
 	@GetMapping("mealplanner/week")
-	public ResponseEntity<WeekResponse> getWeekMeals(String time, String numCalories, String diet,
-			String exclusions, String apiKey) {
+	public ResponseEntity<WeekResponse> getWeekMeals(@RequestParam("timeFrame") String time, @RequestParam("targetCalories") String numCalories, @RequestParam String diet,
+			@RequestParam("exclude") String exclusions, @RequestParam String apiKey) {
 		RestTemplate rt = new RestTemplate();
 		// https://api.spoonacular.com/mealplanner/generate?timeFrame=day
 
@@ -25,7 +28,7 @@ public class PlanController {
 									  .queryParam("targetCalories", numCalories)
 									  .queryParam("diet", diet)
 									  .queryParam("exclude", exclusions)
-									  .queryParam("apiKey", apiKey)
+									  .queryParam( apiKey, "700e9a8811454149bcb551d8ee52aff5")
 									  .build()
 									  .toUri();
 
@@ -34,8 +37,8 @@ public class PlanController {
 	}
 
 	@GetMapping("mealplanner/day")
-	public ResponseEntity<DayResponse> getDayMeals(String time, String numCalories, String diet,
-			String exclusions, String apiKey) {
+	public ResponseEntity<DayResponse> getDayMeals(@RequestParam("timeFrame") String time, @RequestParam("targetCalories") String numCalories, @RequestParam String diet,
+			@RequestParam("exlude") String exclusions, @RequestParam String apiKey) {
 
 		RestTemplate rt = new RestTemplate();
 
@@ -44,7 +47,7 @@ public class PlanController {
 									  .queryParam("targetCalories", numCalories)
 									  .queryParam("diet", diet)
 									  .queryParam("exclude", exclusions)
-									  .queryParam("apiKey", apiKey)
+									  .queryParam("apiKey", Key )
 									  .build()
 									  .toUri();
 
