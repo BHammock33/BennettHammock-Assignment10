42862b7d36f3c0a8a48420eddc9ce304195c7e12
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2022-11-30 14:38:39 -0500

    Update Assignment10Application.java

--- src/main/java/com/coderscampus/assignment10/Assignment10Application.java
+++ src/main/java/com/coderscampus/assignment10/Assignment10Application.java
@@ -11,15 +11,3 @@ public class Assignment10Application {
 	}
 
 }
-//api key 700e9a8811454149bcb551d8ee52aff5
-/**
- * Params: 
- * timeFrame type String example day/week
- * targetCalories: number/integer 2000 caloric target for the day
- * diet type String ex vegetarian 
- * exclude type String ex shellfish/olives 
- * quereyParam("apikey", "700e9a8811454149bcb551d8ee52aff5")
- * 
- * 
- * /
- */
